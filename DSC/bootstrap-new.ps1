1afbb0e76bbfc52d414f6aa6bc6c918e259072e5
jyonke <jon.yonke@gmail.com>
2021-06-23 15:26:38 -0400

    Updated PP logic

--- DSC/bootstrap-new.ps1
+++ DSC/bootstrap-new.ps1
@@ -10,8 +10,16 @@ $cChocoExData = @{
     SettingsURI = 'https://raw.githubusercontent.com/jyonke/chocolatey/Module/DSC/configurations/examples/cChocoBootstrapExample.psd1' 
     RandomDelay = $true
 }
+
+$PackageProviderData = @{
+    Name           = 'Nuget'
+    MinimumVersion = 2.8.5.201
+    Force          = $true
+}
 #NuGet Provider Setup
-Get-PackageProvider -Name Nuget -Force -ForceBootstrap
+if (-not(Get-PackageProvider -Name $PackageProviderData.Name -ErrorAction SilentlyContinue)) {
+    Install-PackageProvider @PackageProviderData
+}
 
 #Register PSRepository
 if (-not(Get-PSRepository -Name $RepositoryData.Name -ErrorAction SilentlyContinue)) {
