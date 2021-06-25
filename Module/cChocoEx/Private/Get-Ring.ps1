5f4cf3c293358d3a52b43ba8b145edc4cd306589
jyonke <jon.yonke@gmail.com>
2021-06-25 00:01:27 -0400

    added more options

--- Module/cChocoEx/Private/Get-Ring.ps1
+++ Module/cChocoEx/Private/Get-Ring.ps1
@@ -2,10 +2,13 @@ function Get-Ring {
     $RegValue = (Get-ItemProperty -Path "HKLM:\Software\Chocolatey\cChoco" -ErrorAction SilentlyContinue).Ring
 
     switch ($RegValue) {
+        "Preview" { $Ring = "preview" }
         "Canary" { $Ring = 'canary' }
+        "Pilot" { $Ring = "pilot" }
         "Fast" { $Ring = 'fast' }
         "Slow" { $Ring = 'slow' }
-        Default { $Ring = $null }
+        "Broad" { $Ring = 'broad' }
+        Default { $Ring = "broad" }
     }
     if ($Ring) {
         Write-Log -Severity 'Information' -Message "Machine Ring: $Ring"
--- Module/cChocoEx/Private/Get-RingValue.ps1
+++ Module/cChocoEx/Private/Get-RingValue.ps1
@@ -6,9 +6,12 @@ function Get-RingValue {
         $Name
     )
     switch ($Name) {
-        "canary" { $Value = 4 }
+        "preview" { $Value = 5 }
+        "canary" { $Value = 5 }
+        "pilot" { $Value = 4 }
         "fast" { $Value = 3 }
         "slow" { $Value = 2 }
+        "broad" { $Value = 1 }
         Default { $Value = 0 }
     }
     return [int]$Value
