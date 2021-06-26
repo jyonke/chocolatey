83382f979ef8cff14ebb9c15cb177c46ed16a879
jyonke <jon.yonke@gmail.com>
2021-06-26 17:56:06 -0400

    Streamlined

--- DSC/bootstrap-new.ps1
+++ DSC/bootstrap-new.ps1
@@ -1,47 +1,31 @@
 
-$RepositoryData = @{
-    Name                      = 'nuget.lvl12.com'
-    SourceLocation            = 'https://nuget.lvl12.com/repository/nuget-ps/'
-    InstallationPolicy        = 'Trusted'
-    PackageManagementProvider = 'nuget'
-}
+$NuGetRepositoryName = 'nuget.lvl12.com'
+$NugetRepositoryURI = 'https://nuget.lvl12.com/repository/nuget-ps/'
 
-$cChocoExData = @{
+$cChocoExParamters = @{
     SettingsURI = 'https://raw.githubusercontent.com/jyonke/chocolatey/Module/DSC/configurations/examples/cChocoBootstrapExample.psd1' 
     RandomDelay = $true
 }
 
-$PackageProviderData = @{
-    Name           = 'Nuget'
-    MinimumVersion = 2.8.5.201
-    Force          = $true
-}
 #NuGet Provider Setup
-if (-not(Get-PackageProvider -Name $PackageProviderData.Name -ErrorAction SilentlyContinue)) {
-    Install-PackageProvider @PackageProviderData
-}
+Install-PackageProvider -Name NuGet -Force
 
 #Register PSRepository
-if (-not(Get-PSRepository -Name $RepositoryData.Name -ErrorAction SilentlyContinue)) {
-    Register-PSRepository @RepositoryData
+$RepositoryData = @{
+    Name                      = $NuGetRepositoryName
+    SourceLocation            = $NugetRepositoryURI
+    InstallationPolicy        = 'Trusted'
+    PackageManagementProvider = 'nuget'
 }
+Register-PSRepository @RepositoryData
 
 #Install and Update cChoco and cChocoEx
-if (Get-Module -Name 'cChoco') {
-    Update-Module -Name 'cChoco'
-}
-else {
-    Install-Module -Name 'cChoco' -Scope 'AllUsers' -Repository $RepositoryData.Name -Force
-}
+if (Get-Module -Name 'cChoco') {Update-Module -Name 'cChoco'}
+else {Install-Module -Name 'cChoco' -Scope 'AllUsers' -Repository $RepositoryData.Name -Force}
 
-if (Get-Module -Name 'cChocoEx') {
-    Update-Module -Name 'cChocoEx'
-}
-else {
-    Install-Module -Name 'cChocoEx' -Scope 'AllUsers' -Repository $RepositoryData.Name -Force
-}
-
+if (Get-Module -Name 'cChocoEx') {Update-Module -Name 'cChocoEx'}
+else {Install-Module -Name 'cChocoEx' -Scope 'AllUsers' -Repository $RepositoryData.Name -Force}
 Import-Module -Name 'cChocoEx' -Force
 
 #Run cChocoEx
-Start-cChocoEx @cChocoExData 
+Start-cChocoEx @cChocoExParamters 
