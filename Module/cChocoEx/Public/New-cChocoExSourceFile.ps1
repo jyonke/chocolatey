f1a18b463b52f03d1a426cde0d8168885634aa6c
jyonke <jon.yonke@gmail.com>
2021-07-15 11:55:50 -0400

    New Function

---
+++ Module/cChocoEx/Public/New-cChocoExSourceFile.ps1
@@ -0,0 +1,82 @@
+function New-cChocoExSourceFile {
+    [CmdletBinding()]
+    param (
+        # Path of Output File
+        [Parameter(Mandatory)]
+        [string]
+        $Path
+    )
+    
+    begin {
+        #Gather Requested Values
+
+        $ExportString = "@{`n"
+        $Absent = New-Object System.Management.Automation.Host.ChoiceDescription '&Absent'
+        $Present = New-Object System.Management.Automation.Host.ChoiceDescription '&Present'
+        $SelectTrue = New-Object System.Management.Automation.Host.ChoiceDescription '&True'
+        $SelectFalse = New-Object System.Management.Automation.Host.ChoiceDescription '&False'
+        $SelectYes = New-Object System.Management.Automation.Host.ChoiceDescription '&Yes'
+        $SelectNo = New-Object System.Management.Automation.Host.ChoiceDescription '&No'
+        $EnsureOptions = [System.Management.Automation.Host.ChoiceDescription[]]($Present, $Absent)
+        $TrueFalseOptions = [System.Management.Automation.Host.ChoiceDescription[]]($SelectTrue, $SelectFalse)
+        $YesNoOptions = [System.Management.Automation.Host.ChoiceDescription[]]($SelectYes, $SelectNo)
+        $Title = 'cChocoEx - Desired State'
+        $ReqChoices = @(
+            'Name'
+            'Ensure (Present/Absent)'
+        )
+        $Optchoices = @(
+            'Priority'
+            'Source'
+            'User'
+            'Password'
+            'KeyFile'
+            'VPN ($True/$False)'
+        )
+    }
+    
+    process {
+        do {
+            $HashTable = $null
+            $HashTable = $host.ui.Prompt(($Title + " - Sources"), $null, $Reqchoices)
+
+            $ExportString += "`t`'$($HashTable.Name)`' = @{`n"
+            $ExportString += "`t`tName`t`t= `'$($HashTable.Name)`'`n"
+            $ExportString += "`t`tEnsure`t`t= `'$($HashTable.('Ensure (Present/Absent)'))`'`n"
+
+            if ($HashTable.('Ensure (Present/Absent)') -eq 'Absent') {
+            }
+            else {
+                #Options
+                $HashTable += $host.ui.Prompt($null, $null, $Optchoices)
+                if ($HashTable.Priority) {
+                    $ExportString += "`t`tPriority`t= `'$($HashTable.Priority)`'`n"
+                }
+                if ($HashTable.Source) {
+                    $ExportString += "`t`tSource`t`t= `'$($HashTable.Source)`'`n"
+                }
+                if ($HashTable.User) {
+                    $ExportString += "`t`tUser`t`t= `'$($HashTable.User)`'`n"
+                }
+                if ($HashTable.Password) {
+                    $ExportString += "`t`tPassword`t= `'$($HashTable.Password)`'`n"
+                }
+                if ($HashTable.KeyFile) {
+                    $ExportString += "`t`tKeyFile`t`t= `'$($HashTable.KeyFile)`'`n"
+                }
+                if ($HashTable.('VPN ($True/$False)')) {
+                    $ExportString += "`t`tVPN`t`t= $($HashTable.('VPN ($True/$False)'))`n"
+                }
+            }
+            $ExportString += "`t}`n"
+
+            $Finished = $host.ui.PromptForChoice($null, 'Finished?', $YesNoOptions, 0)
+
+        } until ($Finished -eq 0) 
+        $ExportString += "`n}"
+    }
+    
+    end {
+        $ExportString | Set-Content -Path $Path
+    }
+}
